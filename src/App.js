b4b27b7c496ce2b0da436cf984a794c9015a3d41
Hritik Dubey <123717071+hritdubey2003@users.noreply.github.com>
2024-02-26 05:13:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Weatherapp from './Components/Weatherapp/weatherapp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Weatherapp/>
+    </div>
+  );
+}
+
+export default App;
